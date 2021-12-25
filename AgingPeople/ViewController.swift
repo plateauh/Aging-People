35d5dd2f37809196fd8c87a75d0636e83fb2ddac
plateauh <70834431+plateauh@users.noreply.github.com>
2021-12-25 23:13:57 +0300

    Trying to add subtitles

--- AgingPeople/ViewController.swift
+++ AgingPeople/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var people = ["George", "Betty", "Fran", "Joe", "Helda", "Winifred", "Zed", "Sara", "Jeffy", "Abraham", "Anna", "Melinda"]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+//        tableView.delegate = self
     }
 
+}
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return people.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "peopleCell", for: indexPath)
+        cell.textLabel?.text = people[indexPath.row]
+        cell.detailTextLabel?.text = generateRandomAge()
+        return cell
+    }
+    
+//    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+//        cell.detailTextLabel?.text = generateRandomAge()
+//    }
+    
+    func generateRandomAge() -> String {
+        return "\(Int.random(in: 5...95)) years old"
+    }
+    
 }
 
